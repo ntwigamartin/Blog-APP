4fa04d7d7af3138486649edb5745af23f98df534
Ntwiga <ntwiga.martin@gmail.com>
2025-06-18 21:46:46 +0300

    project seetings update

--- chat_web/settings.py
+++ chat_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ntwiga.martin@gmail.com'
-EMAIL_HOST_PASSWORD = 'cufz gsbf leln ryyk'
+EMAIL_HOST_USER = config("HOST_USERNAME")
+EMAIL_HOST_PASSWORD = config("HOST_PASSWORD")
